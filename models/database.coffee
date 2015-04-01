5f2d872a2315189cb709f78e5af344c7bdcb2f1d
Patrice PETIT <patrice@coqsenpate.com>
2015-04-01 19:21:02 +0200

    EOW.

--- models/database.coffee
+++ models/database.coffee
@@ -1,6 +1,31 @@
 pg = require('pg')
 connectionString = process.env.DATABASE_URL || "postgres://Patrice:patman00@localhost:5432/HappyCloud"
 
+pg.connect connectionString, (err,client,done)->
+	results = []
+	query = client.query 'SELECT * FROM "User"'
+	#query.on 'row',(row, result)->
+		#console.log 'row', row , 'results : ',result
+	#	result.addRow(row)
+	query.on "end", (result)->
+		console.log JSON.stringify(result.rows, null, "    ")
+		client.end()
+		done(results)
+
+
+		# Stream results back one row at a time
+	query.on 'row', (row,result)->
+		#console.log row, result
+		result.addRow(row)
+		results.push(row)
+
+        #After all data is returned, close connection and return results
+
+        # Handle Errors
+		if(err)
+			console.log(err)
+
+###
 client = new pg.Client(connectionString)
 client.connect (err)->
 	if err
@@ -11,3 +36,4 @@ client.connect (err)->
 			return console.error 'error running query', err
 		console.log result
 		client.end()
+###
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,38 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
 
-router.get ('/AllUsers', function(req, res, next){
+var connectionString = process.env.DATABASE_URL || "postgres://Patrice:patman00@localhost:5432/HappyCloud";
+router.post ('/AllUsers', function(req, res, next){
 	var results = [];
 	// Grab data from http request
 	var data = {text: req.body.text, complete: false};
 
-	// Get a Postgres client from the connection pool
+	    pg.connect(connectionString, function(err, client, done) {
 
-	res.send('respond with a resource');
+        // SQL Query > Insert Data
+        //client.query("INSERT INTO Organisation(text, complete) values($1, $2)", [data.text, data.complete]);
+
+        // SQL Query > Select Data
+        var query = client.query('SELECT * FROM "User"');
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+            // done();
+        });
+
+        // Handle Errors
+        if(err) {
+          console.log(err);
+        }
+    });
+
 });
 module.exports = router;
